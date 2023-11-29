91480b4ffede85807fa47b91b546bdfc0519de0c
Yuki Watanabe <ukwhatn@gmail.com>
2023-11-29 15:10:15 +0900

    initial commit

---
+++ src/main/java/com/isp2/b02/B02Application.java
@@ -0,0 +1,13 @@
+package com.isp2.b02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B02Application.class, args);
+    }
+
+}
